78862398fe079f36e2effbbb6adb705e32da2e45
rslmason <rslmason@outlook.com>
2022-05-12 10:42:04 -0400

    using process.env.OPENAI_API_KEY directly in console.log

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,7 @@
-const { OPENAI_API_KEY } = process.env;
+// const { OPENAI_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
-    console.log(OPENAI_API_KEY);
+    console.log(process.env.OPENAI_API_KEY);
     return {
         statusCode: 200,
         body: JSON.stringify({
