8682d23080fe99bee2fe82c0e07b9ade0468f07f
rslmason <rslmason@outlook.com>
2022-05-12 09:54:57 -0400

    json logging a string literal inside function

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     const { OPENAI_API_KEY } = process.env
-    console.log(OPENAI_API_KEY)
+    console.log('fii')
     return {
         statusCode: 200,
         body: JSON.stringify({
