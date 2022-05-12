fb993d9060348e2aa6c20f2fa7a9def398f74d82
rslmason <rslmason@outlook.com>
2022-05-12 09:53:06 -0400

    json console logging variable

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,6 @@
 exports.handler = async (event, context) => {
     const { OPENAI_API_KEY } = process.env
+    console.log(OPENAI_API_KEY)
     return {
         statusCode: 200,
         body: JSON.stringify({
