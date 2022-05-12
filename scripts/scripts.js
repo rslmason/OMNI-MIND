ab1ab8b5ebb418008736b7a287d6ea04a78d0528
rslmason <rslmason@outlook.com>
2022-05-12 12:25:19 -0400

    snippet and deobfuscator

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,35 @@
 document.getElementById('myButton').disabled = true; // If you just disable this in the HTML, it won't necessarily be disabled on page reload.
+const maskString = 'Open the pod bay doors, Hal.';
+let apiKey = deobfuscator(maskedKey, maskString);
 
-// const apiKey = fetch('netlify/functions/getEnvVar');
+function deobfuscator (strOne, strTwo) {
+    let strArray = [];
+    for (let i = 0; i < strOne.length; i++) {
+        let j = i % strTwo.length;
+        strArray.push(strOne.charCodeAt(i) - strTwo.charCodeAt(j));
+    }
+    return String.fromCharCode(...strArray)
+}
 
-// let apiKey;
+for (i of document.getElementsByTagName('input')) {
+    i.addEventListener('change',(event)=>{
+        const target = event.target;
+        if (target.value === "" || parseFloat(target.value) < target.min) {
+            target.value = target.min;
+        }
+        else if (parseFloat(target.value) > target.max) {
+            console.log('that');
+            target.value = target.max;
+        }
+    })
+    if (i.step == '1') {
+        i.addEventListener('change', (event) => {
+            const target = event.target;
+            target.value = parseInt(target.value);
 
-// fetch(".netlify/functions/api")
-// .then(response => response.json())
-// .then(jason => console.log(jason.myKey));
-
-console.log(apiKey)
-
-// for (i of document.getElementsByTagName('input')) {
-//     i.addEventListener('change',(event)=>{
-//         const target = event.target;
-//         if (target.value === "" || parseFloat(target.value) < target.min) {
-//             target.value = target.min;
-//         }
-//         else if (parseFloat(target.value) > target.max) {
-//             console.log('that');
-//             target.value = target.max;
-//         }
-//     })
-//     if (i.step == '1') {
-//         i.addEventListener('change', (event) => {
-//             const target = event.target;
-//             target.value = parseInt(target.value);
-
-//         })
-//     }
-// }
+        })
+    }
+}
 
 let output = document.getElementById('output');
 
@@ -71,34 +72,34 @@ let engineSelect = document.getElementById('engine');
 const requireStrings = ['text'];
 const excludeStrings = ['search','code','similarity','edit','insert'];
 
-// getEngines().then(r => {
-//     r.data.forEach(i => {
-//         const name = i.id;
-//         if (requireStrings.every(str => name.includes(str)) && !excludeStrings.some(str => name.includes(str))) {
-//             const option = document.createElement("option");
-//             option.value = i.id;
-//             option.text = i.id;
-//             engineSelect.appendChild(option);
-//         }
-//     });
-//     document.getElementById('myButton').disabled = false;
-//     }
-// )
+getEngines().then(r => {
+    r.data.forEach(i => {
+        const name = i.id;
+        if (requireStrings.every(str => name.includes(str)) && !excludeStrings.some(str => name.includes(str))) {
+            const option = document.createElement("option");
+            option.value = i.id;
+            option.text = i.id;
+            engineSelect.appendChild(option);
+        }
+    });
+    document.getElementById('myButton').disabled = false;
+    }
+)
 
-// async function postPrompt(data, engine = 'text-curie-001') {
-//     const response = await fetch(`https://api.openai.com/v1/engines/${engine}/completions`,
-//         {
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 //Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-//                 Authorization: `Bearer ${apiKey}`,
-//             },
-//             body: JSON.stringify(data),
-//         }
-//     );
-//     return response.json();
-// }
+async function postPrompt(data, engine = 'text-curie-001') {
+    const response = await fetch(`https://api.openai.com/v1/engines/${engine}/completions`,
+        {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                //Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify(data),
+        }
+    );
+    return response.json();
+}
 
 
 document.getElementById('myButton').addEventListener('click', (event)=>{
