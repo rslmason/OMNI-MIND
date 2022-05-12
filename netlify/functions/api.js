43c8380b95385b6c3eab15105102e444cb2f71d6
rslmason <rslmason@outlook.com>
2022-05-12 10:28:21 -0400

    added env build variable

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,8 @@
-const myKey = process.env.OPENAI_API_KEY;
+const { OPENAI_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
     
-    console.log(myKey);
+    console.log(OPENAI_API_KEY);
     return {
         statusCode: 200,
         body: JSON.stringify({
