d52232923f3a2c5da9f7ea6a0a174efb9c4df79f
rslmason <rslmason@outlook.com>
2022-05-12 09:46:38 -0400

    json logging myKey with test string

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,14 @@
 const { OPENAI_API_KEY } = process.env
 
+if (OPENAI_API_KEY) {
+    myString = 'fii'
+}
+
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-        myKey: 'foo'
+        myKey: myString
         }),
     }
 }
