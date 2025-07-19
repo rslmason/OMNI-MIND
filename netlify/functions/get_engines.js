a68e848f8552bc2e4813e120d00ffd8963e19f6c
rslmason <rslmason@outlook.com>
2025-07-19 15:02:57 -0400

    get engines with netlify function and key

---
+++ netlify/functions/get_engines.js
@@ -0,0 +1,11 @@
+export default async function (req, context) {
+  const $OPEN_AI_API_KEY = Netlify.env.get("OPEN_AI_API_KEY");
+  return await fetch (
+    'https://api.openai.com/v1/engines',
+    {
+      headers: {
+        Authorization: `Bearer ${$OPEN_AI_API_KEY}`,
+      }
+    }
+  )
+}
