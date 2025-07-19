b23518c488f8328a977ca1819987c4f7e99a6d62
rslmason <rslmason@outlook.com>
2025-07-19 11:16:03 -0400

    env.get instead of env

--- netlify/functions/call.mjs
+++ netlify/functions/call.mjs
@@ -1,4 +1,4 @@
 export default async function (req, context) {
-  const $foobarbaz = Netlify.env("FOOBARBAZ");
+  const $foobarbaz = Netlify.env.get("FOOBARBAZ");
   return new Response("Hellow Orld: " + $foobarbaz);
 }
