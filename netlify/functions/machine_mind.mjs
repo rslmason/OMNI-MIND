147be45ae9c2a66c0989aa586313fc8abacade01
rslmason <rslmason@outlook.com>
2025-07-19 21:04:21 -0400

    revert

--- netlify/functions/machine_mind.mjs
+++ netlify/functions/machine_mind.mjs
@@ -1,21 +1,21 @@
 export default async function (req, context) {
-  // const OPEN_AI_API_KEY = Netlify.env.get("OPEN_AI_API_KEY");
-  // const params = JSON.parse(req.body);
+  const OPEN_AI_API_KEY = Netlify.env.get("OPEN_AI_API_KEY");
   try {
-  //   const response = await fetch (
-  //     `https://api.openai.com/v1/engines/${params.engine}/completions`,
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${OPEN_AI_API_KEY}`,
-  //       },
-  //       body: JSON.stringify(this.params),
-  //     }
-  //   );
-  //   const json = await response.json();
-  //   return new Response(json.choices[0].text.trim());
-    return new Response(req.body)
+    const params = JSON.parse(req.body);
+    const response = await fetch (
+      `https://api.openai.com/v1/engines/${params.engine}/completions`,
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPEN_AI_API_KEY}`,
+        },
+        body: JSON.stringify(this.params),
+      }
+    );
+    const json = await response.json();
+    return new Response(json.choices[0].text.trim());
+    // return new Response(req.body)
   }
   catch {
     return new Response("[Error: My creator is an idiot ]")
